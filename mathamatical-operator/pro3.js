1903f3e96b1ff0b56e491064960bb54f95a1cc04
gaubadalapure <gaubadalapure@gmail.com>
2022-09-06 22:00:43 +0530

    mathamatical operator

---
+++ mathamatical-operator/pro3.js
@@ -0,0 +1,9 @@
+
+let one=1
+let two=2
+let three=3
+let four=4
+let five=5
+let six=6
+//After updating the values, the values stored become one = 1, two = 4, three = 9 , four = 12, five = 10, six = 12
+console.log(one+(two*2)+(three*3)+(four*3)+(five*2)+(six*2));
